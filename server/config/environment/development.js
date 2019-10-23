7eff6f5a55077bbecbc4ceb3b345933a5ff0afe6
salahnasr <salah.nasr@esprit.tn>
2019-10-23 10:57:37 +0800

    Tourisme_Medical_Kawami

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://anonyme:anonyme@ds131890.mlab.com:31890/medical-dev'
+  },
+
+  seedDB: true
+};
